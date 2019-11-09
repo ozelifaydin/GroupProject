04af1e61a730a8c1ba073fc8d25e28e57acaef70
ozelifaydin <57552372+ozelifaydin@users.noreply.github.com>
2019-11-09 15:02:15 -0600

    brunch practice

--- src/GitHubIntro/BranchIntor.java
+++ src/GitHubIntro/BranchIntor.java
@@ -3,6 +3,9 @@ package GitHubIntro;
 public class BranchIntor {
     public static void main(String[] args) {
         System.out.println("hello world");
-        
+
+        int a=5;
+        System.out.println(a);
+
     }
 }
